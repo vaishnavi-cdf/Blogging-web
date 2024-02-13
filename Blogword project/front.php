053c308f88d4bfe7fb557c7e111d11b4df27ea7c
Vaishnavi Mishra <76900158+vaishnavi-cdf@users.noreply.github.com>
2024-02-13 22:05:13 +0530

    Add files via upload

---
+++ Blogword project/front.php
@@ -0,0 +1,24 @@
+<?php
+    if ( ! defined( 'ABSPATH' ) )
+         exit;
+?>
+<style>
+    #blmw{
+        position: fixed;
+        right:0;
+        bottom:0;
+        background-color: #F1E829;
+    }
+    #blmw p {
+        margin: 7px 15px;
+        font-size: 18px;
+        font-weight: 900;
+        text-transform: uppercase;
+    }
+    <img>assets/banner-772x250.jpg
+    assets/icon-256x256.png</img>
+</style>
+
+<div id="blmw">
+    <p><?php echo $blmw_message?></p>
+</div>
